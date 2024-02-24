6b86941526af2868b949e16271fb1d2420be0502
franck-237 <franckkamdem260104@gmail.com>
2024-02-24 09:47:10 +0100

    With ReactJs it's very nice

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Prods from './components/Products/Prods';
+import Banner from './components/Banner/Banner';
+import Suscribe from './components/Suscribe/Suscribe';
+import Testimonials  from './components/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
+import Popup from './components/Popup/Popup';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from './components/TopProducts/TopProducts';
+
+const App = () => {
+
+  const [orderPopup, setOrderPopup] = useState(false);
+
+  const handlerOrderPopup = () => { 
+    setOrderPopup(!orderPopup);
+  };
+
+  useEffect(() => { 
+      AOS.init({
+        offset: 100,
+        duration: 800,
+        easing: "ease-in-sine",
+        delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handlerOrderPopup={handlerOrderPopup}/>
+      <Hero handlerOrderPopup={handlerOrderPopup}/>
+      <Prods />
+      <TopProducts handlerOrderPopup={handlerOrderPopup}/>
+      <Banner />
+      <Suscribe />
+      <Prods />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup}/>
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/Suscribe/Suscribe.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Banner from '../../assets/img/orange-pattern.jpg';
+
+const BannerImg = {
+    backgroundImage: `url(${Banner})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%",
+}; 
+
+const Suscribe = () => {
+  return (
+    <div
+    data-aos="zoom-in"
+    className="mb-20 bg-gray-100 dark:bg-gray-800 text-white"
+    style={BannerImg}
+    >
+      <div className="container backdrop-blur-sm py-10">
+        <div className="space-y-6 max-w-xl mx-auto">
+            <h1
+            data-aos="fade-up"
+            className="text-2xl !text-center sm:text-left sm:text-4xl font-semibold"
+            >Get Notified About New Products</h1>
+            <input 
+            data-aos="fade-up"
+            className="w-full p-3 border-solid border-1 rounded-xl"
+            placeholder='Enter your email'
+            type="text" />
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Suscribe;
